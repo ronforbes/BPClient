755e0054af5b4b82e375b59490317bb8e82bebb7
Ron Forbes <ronforbes528@gmail.com>
2017-07-10 05:43:54 -0700

    Core game mechanic
    * Defined Blocks
    * Implemented block clearing
    * Implemented block emptying
    * Implemented block falling
    * Implemented block matching
    * Implemented block rendering
    * Implemented block sliding
    * Defined Board
    * Implemented board control
    * Implemented board gravity
    * Implemented match detection
    * Created a pixel sprite

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	public int X, Y, Type;
+	public const int TypeCount = 6;
+
+	public enum BlockState
+    {
+        Empty,
+        Idle,
+        Sliding,
+        WaitingToFall,
+        Falling,
+        Matched,
+        WaitingToClear,
+        Clearing,
+        WaitingToEmpty,
+    }
+
+	public BlockState State;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/BoardGravity.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGravity : MonoBehaviour {
+	Board board;
+    MatchDetector matchDetector;
+
+    // Use this for initialization
+    void Awake()
+    {
+        board = GetComponent<Board>();
+        matchDetector = GameObject.Find("Board").GetComponent<MatchDetector>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        for (int x = 0; x < Board.Columns; x++)
+        {
+            bool emptyBlockDetected = false;
+
+            for (int y = 0; y < Board.Rows; y++)
+            {
+                if (board.Blocks [x, y].State == Block.BlockState.Empty)
+                {
+                    emptyBlockDetected = true;
+                }
+
+                if (board.Blocks [x, y].State == Block.BlockState.Idle && emptyBlockDetected)
+                {
+                    board.Blocks [x, y].GetComponent<BlockFaller>().Target = board.Blocks [x, y - 1];
+                    board.Blocks [x, y].GetComponent<BlockFaller>().Fall();
+                }
+
+                if (board.Blocks [x, y].GetComponent<BlockFaller>().JustFell)
+                {
+                    if (y > 0 && (board.Blocks [x, y - 1].State == Block.BlockState.Empty || board.Blocks [x, y - 1].State == Block.BlockState.Falling))
+                    {
+                        board.Blocks [x, y].GetComponent<BlockFaller>().Target = board.Blocks [x, y - 1];
+                        board.Blocks [x, y].GetComponent<BlockFaller>().ContinueFalling();
+                    } else
+                    {
+                        board.Blocks [x, y].State = Block.BlockState.Idle;
+
+                        matchDetector.RequestMatchDetection(board.Blocks [x, y]);
+                    }
+
+                    board.Blocks [x, y].GetComponent<BlockFaller>().JustFell = false;
+                }
+            }
+        }
+
+		// Add new blocks on the top of the board
+		for(int x = 0; x < Board.Columns; x++) {
+			if(board.Blocks[x, Board.Rows - 1].State == Block.BlockState.Empty) {
+				int type;
+
+				do {
+					type = Random.Range(0, Block.TypeCount);
+				} while((x != 0 && board.Blocks[x - 1, Board.Rows - 1].Type == type) || board.Blocks[x, Board.Rows - 1].Type == type);
+
+				board.Blocks[x, Board.Rows - 1].Type = type;
+				
+				if(board.Blocks[x, Board.Rows - 2].State == Block.BlockState.Idle) {
+					board.Blocks[x, Board.Rows - 1].State = Block.BlockState.Idle;
+				}
+
+				if(board.Blocks[x, Board.Rows - 2].State == Block.BlockState.Empty || board.Blocks[x, Board.Rows - 2].State == Block.BlockState.Falling) {
+					board.Blocks[x, Board.Rows - 1].GetComponent<BlockFaller>().Target = board.Blocks[x, Board.Rows - 2];
+					board.Blocks[x, Board.Rows - 1].GetComponent<BlockFaller>().ContinueFalling();
+				}
+			}
+		}
+    }
+}
