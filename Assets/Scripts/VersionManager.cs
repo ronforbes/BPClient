64208c89590a96bacc4b9b11ff4e9ac5751b16d2
Ron Forbes <ronforbes528@gmail.com>
2017-07-13 19:54:34 -0400

    Implemented version label

---
+++ Assets/Scripts/VersionManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionManager : MonoBehaviour {
+	static VersionManager instance;
+    public static VersionManager Instance
+    {
+        get
+        {
+            // Get the singleton instance of the Game Clock
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<VersionManager>();
+
+                DontDestroyOnLoad(instance.gameObject);
+            }
+
+            return instance;
+        }
+    }
+	
+	public int Major;
+	public int Minor;
+	public int Patch;
+
+	Text versionLabel;
+
+	void Awake() {
+		versionLabel = GetComponent<Text>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		versionLabel.text = "Version " + Major + "." + Minor + "." + Patch;
+	}
+}
---
+++ Assets/Scripts/VersionRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionRenderer : MonoBehaviour {
+	Text versionLabel;
+
+	void Awake() {
+		versionLabel = GetComponent<Text>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		versionLabel.text = "Version " + VersionManager.Instance.Major + "." + VersionManager.Instance.Minor + "." + VersionManager.Instance.Patch;
+	}
+}
